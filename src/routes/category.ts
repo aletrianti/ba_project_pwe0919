8d015cb5f2cacbdf0a4a215a151cb52978f3d160
Jose <josecarlosesparza@gmail.com>
2021-01-11 18:27:46 +0100

    [/category]

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -27,16 +27,51 @@ router.post('/', async (req: Request, res: Response, next) => {
     const categoryExists: ICategory = await knex('department').where('name', newCategoryName).first();
     if (categoryExists) throw Error(`${categoryExists.name} already exists for company: ${companyId}`);
 
-    const newCategory = await knex('role').insert({
+    const newCategory = await knex('category').insert({
       name: newCategoryName,
       companyId: companyId,
       createdAt: dateDB(),
       updatedAt: dateDB(),
     });
 
-    const category = await knex('role').where('ID', newCategory).first();
+    const category = await knex('category').where('ID', newCategory).first();
 
     Api.sendSuccess<ICategory>(req, res, category);
+  } catch (err) {
+    Api.sendError(req, res, err);
+  }
+});
+
+router.post('/update', async (req: Request, res: Response, next) => {
+  try {
+    const { userId, companyId } = getUserIds(req);
+    if (!userId) throw new Error('User does not exists');
+    if (!companyId) throw new Error('User not assigned to a company');
+
+    const body: Partial<ICategory> = req.body;
+    const { ID, ...reqbody } = body;
+
+    const updatedCategory = await knex('category').where('ID', Number(ID)).update(reqbody);
+
+    const category: ICategory = await knex('category').where('ID', updatedCategory).first();
+
+    Api.sendSuccess<ICategory>(req, res, category);
+  } catch (err) {
+    Api.sendError(req, res, err);
+  }
+});
+
+router.post('/delete', async (req: Request, res: Response, next) => {
+  try {
+    const { userId, companyId } = getUserIds(req);
+    if (!userId) throw new Error('User does not exists');
+    if (!companyId) throw new Error('User not assigned to a company');
+
+    const ID = req.body;
+
+    await knex('category').where('ID', Number(ID)).del();
+
+    Api.sendSuccess<number>(req, res, ID);
   } catch (err) {
     Api.sendError(req, res, err);
   }
