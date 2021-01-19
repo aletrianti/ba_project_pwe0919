9380fcb351f1dbc4d6f595090f46277f7b3b6f22
Alessia Trianti <alestrianti@gmail.com>
2021-01-19 17:54:19 +0100

    Remove prevent default (wip)

--- client/src/components/AdminPanel/Achievements/AchievementsForms/AchievementsForms.tsx
+++ client/src/components/AdminPanel/Achievements/AchievementsForms/AchievementsForms.tsx
@@ -150,8 +150,6 @@ class AchievementsForms extends React.Component<AchievementsFormsProps, Achievem
   };
 
   addAchievement = async (event: FormEvent): Promise<void> => {
-    event.preventDefault();
-
     await this.saveAchievementToRedux();
     await this.saveAchievementToDB();
 
@@ -159,8 +157,6 @@ class AchievementsForms extends React.Component<AchievementsFormsProps, Achievem
   };
 
   editAchievement = async (event: FormEvent): Promise<void> => {
-    event.preventDefault();
-
     await this.saveEditedAchievementToDB();
     // await this.saveAchievementToDB();
 
