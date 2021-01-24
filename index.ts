923a95471442e0332206af775cc56524e2664b70
Jose <josecarlosesparza@gmail.com>
2021-01-24 13:36:03 +0100

    Fix heroku

--- index.ts
+++ index.ts
@@ -60,6 +60,13 @@ app.use('/api', router);
 //   })
 // }
 
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // Start app
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
